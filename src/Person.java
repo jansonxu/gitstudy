1edb7487289bfe333cf46c6a815ef2dc7c902f88
jansonxu <jansonxu@sina.cn>
2018-04-19 06:45:55 +0800

    新建了Person类

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+/**
+ * Description：xxx<br/>
+ * Copyright (c) ， 2018， Jansonxu <br/>
+ * This program is protected by copyright laws. <br/>
+ * Date：2018年04月19日
+ *
+ * @author 徐文波
+ * @version : 1.0
+ */
+public class Person {
+}
